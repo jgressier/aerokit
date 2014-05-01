efd45a96b78ccecba94cbd99f00ce0a16d2b3e94
J. Gressier <jeremie.gressier@isae.fr>
2014-05-01 09:30:35 +0000

    thermo functions

--- ThermoKerosen.py
+++ ThermoKerosen.py
@@ -1,14 +1,17 @@
 # python module: ThermoKerosen
 
-#import math
+import math
 
-r_air = 287.04
+r_air = 287.04 # constants are computed relative to air "r" constant
 
 def Cp(T):
     return r_air*(4.47659+(8.01994e-3-1.8373e-6*T)*T)
 
 def Enthalpy(T):
     return r_air*(-149.054+(4.47659+(4.00997e-3-6.12432e-7*T)*T)*T)
+
+def Phi(T):
+    return (4.47659*math.log(T)+(8.01994e-3 - 9.18648e-7*T)*T)/math.log(10.)
 
 def Pc_inf():
     return 10300.*4184
--- ThermoRealGas.py
+++ ThermoRealGas.py
@@ -15,4 +15,8 @@ def Cp(T):
 
 def Enthalpy(T):
     a = math.exp(3090./T)
-    return rconst*((3.5+(-1.4e-5+7.467e-9*T)*T)*T+3090.(a-1.)) 
+    return rconst*((3.5+(-1.4e-5+7.467e-9*T)*T)*T+3090./(a-1.)) 
+
+def Phi(T):
+    a = math.exp(3090./T)
+    return (((3.5*math.log(T)+(-2.8e-5+1.12e-8*T)*T)+3090./T/(a-1.))-math.log((a-1.)/a))/math.log(10.)
