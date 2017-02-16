5df73b5abe63e58cc0ff70043a0554e890f9dd43
j.gressier <jeremie.gressier@isae.fr>
2017-02-16 11:17:19 +0100

    change folder structure for HADES package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hades',
+      version='0.1',
+      description='fluid mechanics, aero and post processing tools',
+      url='',
+      author='ISAE/DAEP',
+      author_email='',
+      license='',
+      packages=['hades'],
+      zip_safe=False)
