7804aa588adef658f1fb741a622044e174982887
j.gressier <jeremie.gressier@isae.fr>
2019-11-21 08:59:45 +0100

    improve shock polar plot functions and style

--- hades/aero/plot/defaultstyle.py
+++ hades/aero/plot/defaultstyle.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 
-def set_grid():
-	plt.minorticks_on()
-	plt.grid(which='major', linestyle='-', alpha=0.8)
-	plt.grid(which='minor', linestyle=':', alpha=0.5)
+sty_shock = { 'color': 'red',    'linewidth': 3 }
+sty_carac = { 'color': 'orange', 'linewidth': 2 }
+sty_wall  = { 'color': 'black',  'linewidth': 3 }
+sty_flow  = { 'color': 'green',  'linewidth': 3 }
+sty_text  = { 'fontsize': 14 }
+
+def set_grid(ax=plt):
+	ax.minorticks_on()
+	ax.grid(which='major', linestyle='-', alpha=0.8)
+	ax.grid(which='minor', linestyle=':', alpha=0.5)
 
 def figure_theta_sigma(**kwargs):
 	fig = plt.figure(**kwargs)
--- hades/aero/plot/shockpolar.py
+++ hades/aero/plot/shockpolar.py
@@ -41,11 +41,11 @@ def plot_theta_sigma(mach, gamma=defg._gamma, npts=100, curve='both', devmax=Fal
     if sonic:
     	thet = sw.dev_Sonic(mach, gamma=gamma)
     	sig  = sw.sigma_Sonic(mach, gamma=gamma)
-    	if curve in ['right', 'both']: plt.plot( thet, sig, 'wo')
-    	if curve in ['left', 'both']:  plt.plot(-thet, sig, 'wo')
+    	if curve in ['right', 'both']: plt.plot( thet, sig, 'ko', markerfacecolor='white')
+    	if curve in ['left', 'both']:  plt.plot(-thet, sig, 'ko', markerfacecolor='white')
 
 
-def plot_theta_pressure(mach, gamma=defg._gamma, npts=100, thet_init=0., p_init=1., curve='both', devmax=False, sonic=False, color='k', linestyle='-', **kwargs):
+def plot_theta_pressure(mach, gamma=defg._gamma, npts=100, thet_init=0., p_init=1., curve='both', devmax=False, sonic=False, color='k', linestyle='-', ax=plt, **kwargs):
     """
     	Plot shock polar curve in deviation / pressure ratio axes
 
@@ -68,19 +68,19 @@ def plot_theta_pressure(mach, gamma=defg._gamma, npts=100, thet_init=0., p_init=
     sig = np.linspace(deg.asin(1./mach), 90., npts+1)
     dev = sw.deflection_Mach_sigma(mach, sig, gamma)
     ps  = p_init * sw.Ps_ratio(mach*deg.sin(sig), gamma)     # pressure ratio only depends on normal Mach number
-    if curve in ['right', 'both']: plt.plot(thet_init+dev, ps, color=color, linestyle=linestyle, **kwargs)
-    if curve in ['left',  'both']: plt.plot(thet_init-dev, ps, color=color, linestyle=linestyle, **kwargs)
+    if curve in ['right', 'both']: ax.plot(thet_init+dev, ps, color=color, linestyle=linestyle, **kwargs)
+    if curve in ['left',  'both']: ax.plot(thet_init-dev, ps, color=color, linestyle=linestyle, **kwargs)
     if devmax:
     	thet = sw.dev_Max(mach, gamma=gamma)
     	sig  = sw.sigma_DevMax(mach, gamma=gamma)
     	ps   = sw.Ps_ratio(mach*deg.sin(sig), gamma=gamma)
-    	if curve in ['right', 'both']: plt.plot(thet_init+thet, p_init*ps, 'ro', alpha=0.9)
-    	if curve in ['left', 'both']:  plt.plot(thet_init-thet, p_init*ps, 'ro', alpha=0.9)
+    	if curve in ['right', 'both']: ax.plot(thet_init+thet, p_init*ps, 'ro', alpha=0.9)
+    	if curve in ['left', 'both']:  ax.plot(thet_init-thet, p_init*ps, 'ro', alpha=0.9)
     if sonic:
     	thet = sw.dev_Sonic(mach, gamma=gamma)
     	sig  = sw.sigma_Sonic(mach, gamma=gamma)
     	ps   = sw.Ps_ratio(mach*deg.sin(sig), gamma=gamma)
-    	if curve in ['right', 'both']: plt.plot(thet_init+thet, p_init*ps, 'wo')
-    	if curve in ['left', 'both']:  plt.plot(thet_init-thet, p_init*ps, 'wo')
+    	if curve in ['right', 'both']: ax.plot(thet_init+thet, p_init*ps,  'ko', markerfacecolor='white')
+    	if curve in ['left', 'both']:  ax.plot(thet_init-thet, p_init*ps,  'ko', markerfacecolor='white')
 
 
