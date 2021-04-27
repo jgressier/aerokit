0c7a7eb2b723091e5160a189457e67c955d7ff58
j.gressier <jeremie.gressier@isae.fr>
2021-04-27 23:28:34 +0200

    decrease matplotlib requirement

---
+++ aerokit/aero/refstate.py
@@ -0,0 +1,17 @@
+"""
+    The ``refstate`` module
+    =========================
+ 
+    Provides integral models for boundary layer computations
+  
+    Available functions
+    -------------------
+ 
+"""
+
+class refstate:
+
+    def __init__(self, **kwargs):
+        self._dict = kwargs
+
+    #def Reynolds(self):
