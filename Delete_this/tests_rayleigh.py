540cd18ea88e42068d094e688159c1b25677f8f5
Pierre Mignerot <mignerot.pierre@gmail.com>
2022-04-13 11:38:24 +0200

    added tests to Mach_TiTicri

---
+++ Delete_this/tests_rayleigh.py
@@ -0,0 +1,63 @@
+import numpy                         as np
+import aerokit.aero.Rayleigh         as ray
+
+"""def test_Ps_Pscri():
+
+def test_Ts_Rscri():
+
+def test_Rho_Rhocri():
+
+def test_Pi_Picri():
+
+def test_V_Vcri():
+
+def test_NormdS():"""
+
+print("Ps_Pscri")
+print(ray.Ps_Pscri(1, 1.4))
+print(ray.Ps_Pscri(1.5, 1.4))
+print(ray.Ps_Pscri(.5, 1.4))
+print(ray.Ps_Pscri(1.5, 1.3))
+print(ray.Ps_Pscri(.5, 1.3))
+
+print("Ts_Tscri")
+print(ray.Ts_Tscri(1, 1.4))
+print(ray.Ts_Tscri(1.5, 1.4))
+print(ray.Ts_Tscri(.5, 1.4))
+print(ray.Ts_Tscri(1.5, 1.3))
+print(ray.Ts_Tscri(.5, 1.3))
+
+"""print("Ti_Ticri")
+print(ray.Ti_Ticri(1, 1.4))
+print(ray.Ti_Ticri(1.5, 1.4))
+print(ray.Ti_Ticri(.5, 1.4))
+print(ray.Ti_Ticri(1.5, 1.3))
+print(ray.Ti_Ticri(.5, 1.3))"""
+
+print("Rho_Rhocri")
+print(ray.Rho_Rhocri(1, 1.4))
+print(ray.Rho_Rhocri(1.5, 1.4))
+print(ray.Rho_Rhocri(.5, 1.4))
+print(ray.Rho_Rhocri(1.5, 1.3))
+print(ray.Rho_Rhocri(.5, 1.3))
+
+print("Pi_Picri")
+print(ray.Pi_Picri(1, 1.4))
+print(ray.Pi_Picri(1.5, 1.4))
+print(ray.Pi_Picri(.5, 1.4))
+print(ray.Pi_Picri(1.5, 1.3))
+print(ray.Pi_Picri(.5, 1.3))
+
+print("V_Vcri")
+print(ray.V_Vcri(1, 1.4))
+print(ray.V_Vcri(1.5, 1.4))
+print(ray.V_Vcri(.5, 1.4))
+print(ray.V_Vcri(1.5, 1.3))
+print(ray.V_Vcri(.5, 1.3))
+
+print("NormdS")
+print(ray.NormdS(1, 1.4))
+print(ray.NormdS(1.5, 1.4))
+print(ray.NormdS(.5, 1.4))
+print(ray.NormdS(1.5, 1.3))
+print(ray.NormdS(.5, 1.3))
