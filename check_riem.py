a58c25e1171ea4630e7e023d88545b4629c6be84
jgressier <jeremie.gressier@isae.fr>
2023-11-21 23:45:02 +0100

    clean up

--- check_riem.py
+++
@@ -1,7 +0,0 @@
-import aerokit.instance.riemann as riem
-import aerokit.aero.unsteady1D as uq
-import numpy as np
-
-qL = uq.unsteady_state(rho=2., u=0., p=10.)
-qR = uq.unsteady_state(rho=1., u=0., p=1.)
-pb = riem.riemann_pb(qL, qR)
